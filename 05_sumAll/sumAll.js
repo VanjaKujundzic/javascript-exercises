dae9fdcad82234f4dea421dcb7c1286a15b08c8f
Vanja Kujundzic <kujundzicv97@gmail.com>
2023-12-18 13:16:13 +0100

    exercise about adding sum numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let sum=0;
+    if(number1<0 || number2<0){
+        return "ERROR";
+    }else if(!Number.isInteger(number1) || !Number.isInteger(number2))
+         return "ERROR"
+
+    if(number1>number2){
+        for(let i=number1;i>=number2;i--){
+            sum+=i;
+    
+            
+        }
+        return sum;
+
+    }
+
+    for(let i=number1;i<=number2;i++){
+        sum+=i;
+
+        
+    }
+    return sum;
 
 };
 
