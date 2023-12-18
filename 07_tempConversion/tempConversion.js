c9250bed102ae72b83f46e615a5e35104f75ef40
Vanja Kujundzic <kujundzicv97@gmail.com>
2023-12-18 14:13:45 +0100

    solved different exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+const toCelsius= (fahrenheit-32)*5/9;
+if(toCelsius===0){
+return toCelsius;
+}
+return +toCelsius.toFixed(1); 
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+const toFahrenheit= (celsius*(9/5)+32);
+
+return +toFahrenheit.toFixed(1);
+
 };
 
 // Do not edit below this line
