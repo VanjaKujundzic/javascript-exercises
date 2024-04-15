66f3ba1e341f96844a0011839ca32dee2920bea7
Vanja Kujundzic <kujundzicv97@gmail.com>
2024-04-16 01:19:32 +0200

    finish an exercise with palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string= string.replace(/\s/g, "");
+    string= string.replace(/!/g, "");
+    string= string.replace(/,/g, "");
+    string = string.replace(/\./g, "");
+    
+    let originalString= string.toLowerCase();
+    const lengthOfLoop= string.length;
+    let lastChar='';
+    for(let i=0; i<lengthOfLoop;i++){
+    lastChar+= string.slice(-1);
+    string = string.slice(0, -1);
+    lastChar= lastChar.toLowerCase();
+       
+    }
+    
+
+    if(originalString===lastChar)
+    return true;
+    else return false;
 
 };
+console.log(palindromes("R3c3car"));
 
 // Do not edit below this line
 module.exports = palindromes;
+
