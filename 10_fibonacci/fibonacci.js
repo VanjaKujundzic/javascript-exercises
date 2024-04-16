e9115ebfa1b961a14920bf40bf0e7173a001869a
Vanja Kujundzic <kujundzicv97@gmail.com>
2024-04-16 13:47:35 +0200

    finish an exercise about creating a fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if(count<0)
+    return "OOPS";
+    const fib=[0,1];
 
+    for (let i = 2; i <= count; i++) {
+        fib[i]= fib[i-1]+fib[i-2];
+
+        
+        
+    }
+    return fib[count];
+
+   
 };
+
+console.log(fibonacci(3)); 
+
+console.log(fibonacci(4));
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
